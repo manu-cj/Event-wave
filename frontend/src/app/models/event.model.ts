dc93b61ff746b1a2cdbe09a895664b2f6c39cba1
manu-cj <carenjotje@gmail.com>
2025-07-18 17:44:17 +0200

    add type for event

--- frontend/src/app/models/event.model.ts
+++ frontend/src/app/models/event.model.ts
@@ -1,3 +1,4 @@
+
 export interface IEvent {
   id?: string;
   title: string;
@@ -6,9 +7,36 @@ export interface IEvent {
   city: string;
   postalCode: string;
   address: string;
-  
+  availablePlaces: number;
+  pictureUrl: string;
+
 }
 
 export interface IEventPage {
-
+  content: IEvent[];
+  pageable: {
+    pageNumber: number;
+    pageSize: number;
+    sort: {
+      empty: boolean;
+      sorted: boolean;
+      unsorted: boolean;
+    };
+    offset: number;
+    paged: boolean;
+    unpaged: boolean;
+  };
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  size: number;
+  number: number;
+  sort: {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+  };
+  numberOfElements: number;
+  first: boolean;
+  empty: boolean;
 }
