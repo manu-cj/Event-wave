b8a624182806b68e73751c964677992b3cb85fb7
manu-cj <carenjotje@gmail.com>
2025-07-23 09:05:12 +0200

    save

---
+++ backend/src/main/java/com/manu/template/repository/ReservationRepository.java
@@ -0,0 +1,9 @@
+package com.manu.template.repository;
+
+import com.manu.template.model.Reservation;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ReservationRepository extends JpaRepository<Reservation, UUID> {
+}
