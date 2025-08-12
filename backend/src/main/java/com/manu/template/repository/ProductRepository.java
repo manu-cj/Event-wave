edca1dfb5c40740678e8322359fd6d51d311b7b8
manu-cj <carenjotje@gmail.com>
2025-08-12 14:16:33 +0200

    Delete: delete product model, dto, mapper, service, controller

--- backend/src/main/java/com/manu/template/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.manu.template.repository;
-
-import com.manu.template.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
